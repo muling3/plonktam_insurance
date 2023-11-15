7864002e4358ff380a6157993139b2a7aa79432a
Alexander Muli <mulingealexmuli@gmail.com>
2023-11-15 17:24:06 +0300

    private vehicles update

--- src/app/service/privateVehicles/comprehensive/cic/page.tsx
+++ src/app/service/privateVehicles/comprehensive/cic/page.tsx
@@ -1,10 +1,10 @@
-const Trident = () => {
+const CIC = () => {
   return (
     <>
       <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
         <div className="header w-full my-4">
           <p className="text-lg font-semibold uppercase">
-            trident comprehensive covers
+            CIC comprehensive covers
           </p>
         </div>
         <div className="opts w-full grid grid-cols-3 gap-16 place-items-center">
@@ -12,26 +12,32 @@ const Trident = () => {
           <div className="font-bold">Insurance cost</div>
           <div className="font-bold">Action</div>
 
-          <div>Below Ksh. 1M upto 15 Years</div>
-          <div className="">ksh. 30, 000</div>
+          <div>Upto Ksh. 1M </div>
+          <div className="">6%</div>
           <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
             add to cart
           </div>
 
-          <div>Below Ksh. 1M upto 16 - 20 Years</div>
-          <div className="">ksh. 30, 000</div>
+          <div>Ksh. 1M To Ksh. 1.5M</div>
+          <div className="">5%</div>
           <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
             add to cart
           </div>
 
-          <div>Above Ksh. 1M upto 15 Years</div>
-          <div className="">ksh. 30, 000</div>
+          <div>Ksh. 1.5M To 2.5M </div>
+          <div className="">4%</div>
           <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
             add to cart
           </div>
 
-          <div>Above Ksh. 1M upto 16 - 20 Years</div>
-          <div className="">ksh. 30, 000</div>
+          <div>Ksh. 2.5M To 5M </div>
+          <div className="">3.5%</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div>Above Ksh. 5M </div>
+          <div className="">3%</div>
           <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
             add to cart
           </div>
@@ -41,4 +47,4 @@ const Trident = () => {
   );
 };
 
-export default Trident;
+export default CIC;
