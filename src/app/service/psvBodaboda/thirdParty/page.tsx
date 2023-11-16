1d0c8e67cefd636e25e3bc1cda300a58d890c17d
Alexander Muli <mulingealexmuli@gmail.com>
2023-11-16 13:47:38 +0300

    psv boda boda all cats

---
+++ src/app/service/psvBodaboda/thirdParty/page.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const ThirdParty = () => {
+  return (
+    <>
+      <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
+        <div className="header w-full my-4">
+          <p className="text-lg font-semibold uppercase">
+            third party psv annual covers
+          </p>
+        </div>
+        <div className="opts w-full grid grid-cols-3 gap-16 place-items-center">
+          <div className="font-bold">Insurance</div>
+          <div className="font-bold">Price</div>
+          <div className="font-bold">Action</div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/trident.png"}
+              alt={"Trident"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 3, 700</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/directline.png"}
+              alt={"Directline"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 4, 500</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ThirdParty;
