b35f4cffc076baaa8a555439574acbdba21b12a7
Alexander Muli <mulingealexmuli@gmail.com>
2023-11-16 13:35:47 +0300

    commercial tuktuks all cats

---
+++ src/app/service/commercialTuktuks/comprehensive/page.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Comprehensive = () => {
+  return (
+    <>
+      <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
+        <div className="header w-full my-4">
+          <p className="text-lg font-semibold uppercase">
+            comprehensive commercial tuktuk annual covers
+          </p>
+        </div>
+        <div className="opts w-full grid grid-cols-3 gap-16 place-items-center">
+          <div className="font-bold">Insurance</div>
+          <div className="font-bold">Price</div>
+          <div className="font-bold">Action</div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/trident.png"}
+              alt={"Trident"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 8, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/monarch.png"}
+              alt={"Amaco"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 22, 500</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/amaco.png"}
+              alt={"Amaco"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 10, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Comprehensive;
