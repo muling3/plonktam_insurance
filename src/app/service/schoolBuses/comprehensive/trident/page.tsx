a0f1ae09436eec164de96dfad80cfece0f5b98ff
Alexander Muli <mulingealexmuli@gmail.com>
2023-11-16 13:27:36 +0300

    school buses all cats

---
+++ src/app/service/schoolBuses/comprehensive/trident/page.tsx
@@ -0,0 +1,44 @@
+const Amaco = () => {
+  return (
+    <>
+      <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
+        <div className="header w-full my-4">
+          <p className="text-lg font-semibold uppercase">
+            trident school bus comprehensive covers
+          </p>
+        </div>
+        <div className="opts w-full grid grid-cols-3 gap-16 place-items-center">
+          <div className="font-bold">Value of the School Buses Insured</div>
+          <div className="font-bold">Insurance Cost</div>
+          <div className="font-bold">Action</div>
+
+          <div>Below Ksh. 1M upto 15 Years</div>
+          <div className="">ksh. 45, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div>Below Ksh. 1M upto 16 - 20 Years</div>
+          <div className="">ksh. 45, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div>Above Ksh. 1M upto 15 Years</div>
+          <div className="">ksh. 43, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div>Above Ksh. 1M upto 16 - 20 Years</div>
+          <div className="">ksh. 43, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Amaco;
---
+++ src/app/service/schoolBuses/thirdParty/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const ShortPeriods = () => {
+  return (
+    <>
+      <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
+        <div className="header w-full my-4">
+          <p className="text-lg font-semibold uppercase">
+            third party school buses
+          </p>
+        </div>
+        <div className="opts w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8">
+          <Link href={"/service/schoolBuses/thirdParty/amaco"}>
+            <div className="py-2 uppercase text-white bg-[#007A37] grid place-items-center">
+              amaco insurance
+            </div>
+          </Link>
+
+          <Link href={"/service/schoolBuses/thirdParty/trident"}>
+            <div className="py-2 uppercase text-white bg-[#007A37] grid place-items-center">
+              trident insurance
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ShortPeriods;
