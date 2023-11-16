d0c7e2170a38dd9b7ee17bca251cf36167506ba0
Alexander Muli <mulingealexmuli@gmail.com>
2023-11-16 13:55:27 +0300

    psv motorcycle

---
+++ src/app/service/psvMotorcycle/comprehensive/page.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+
+const Comprehensive = () => {
+  return (
+    <>
+      <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
+        <div className="header w-full my-4">
+          <p className="text-lg font-semibold uppercase">
+            comprehensive private annual covers
+          </p>
+        </div>
+        <div className="opts w-full grid grid-cols-3 gap-16 place-items-center">
+          <div className="font-bold">Insurance</div>
+          <div className="font-bold">Price</div>
+          <div className="font-bold">Action</div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/trident.png"}
+              alt={"Trident"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 5, 500</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/heritage.png"}
+              alt={"Heritage"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 6, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/monarch.png"}
+              alt={"Monarch"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 7, 000</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/monarch.png"}
+              alt={"Monarch"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 6, 900</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/directline.png"}
+              alt={"DirectLine"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="">ksh. 7, 500</div>
+          <div className="bg-[#007A37] text-white px-4 py-2 uppercase">
+            add to cart
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Comprehensive;
