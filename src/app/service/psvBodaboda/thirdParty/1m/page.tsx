0fb105f5dc176ae79cc9445eaf0f2fa96ba3c8e3
Alexander Muli <mulingealexmuli@gmail.com>
2024-02-18 22:43:09 +0300

    updated psv bodaboda

---
+++ src/app/service/psvBodaboda/thirdParty/1m/page.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const OneMonth = () => {
+  return (
+    <>
+      <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
+        <div className="header w-full my-4 flex justify-between items-center flex-wrap">
+          <p className="text-lg font-semibold uppercase dark:text-gray-900">
+            third party psv 1 Month covers
+          </p>
+          <Link href={"/service/psvBodaboda/thirdParty"}>
+            <span className="material-symbols-outlined p-2 rounded-full border-2 border-[#007A37] dark:text-gray-900">
+              close
+            </span>
+          </Link>
+        </div>
+        <div className="opts w-full grid grid-cols-3 gap-6 sm:gap-16 place-items-center">
+          <div className="font-bold dark:text-gray-900">Insurance</div>
+          <div className="font-bold dark:text-gray-900">Price(Ksh)</div>
+          <div className="font-bold dark:text-gray-900">Action</div>
+
+          <div className="h-16 w-20 relative">
+            <Image
+              src={"/partners/trident.png"}
+              alt={"Trident"}
+              fill
+              priority
+              className="w-full h-full"
+            />
+          </div>
+          <div className="dark:text-gray-900">1, 700</div>
+          <Link href={"/service/psvBodaboda/thirdParty/1m"}>
+            <div className="bg-[#007A37] text-white text-xs sm:text-base px-4 py-4 sm:px-4 sm:py-2 uppercase rounded-md">
+              select
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OneMonth;
--- src/app/service/psvBodaboda/thirdParty/page.tsx
+++ src/app/service/psvBodaboda/thirdParty/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 
 const ThirdParty = () => {
   return (
@@ -7,7 +6,7 @@ const ThirdParty = () => {
       <div className="w-full flex-1 px-2 py-2 sm:px-24 sm:py-4">
         <div className="header w-full my-4 flex justify-between items-center flex-wrap">
           <p className="text-lg font-semibold uppercase dark:text-gray-900">
-            third party psv annual covers
+            PSV BODABODA
           </p>
           <Link href={"/service/psvBodaboda"}>
             <span className="material-symbols-outlined p-2 rounded-full border-2 border-[#007A37] dark:text-gray-900">
@@ -15,40 +14,20 @@ const ThirdParty = () => {
             </span>
           </Link>
         </div>
-        <div className="opts w-full grid grid-cols-3 gap-6 sm:gap-16 place-items-center">
-          <div className="font-bold dark:text-gray-900">Insurance</div>
-          <div className="font-bold dark:text-gray-900">Price(Ksh)</div>
-          <div className="font-bold dark:text-gray-900">Action</div>
-
-          <div className="h-16 w-20 relative">
-            <Image
-              src={"/partners/trident.png"}
-              alt={"Trident"}
-              fill
-              priority
-              className="w-full h-full"
-            />
-          </div>
-          <div className="dark:text-gray-900">3, 700</div>
-          <Link href={"/service/psvBodaboda/thirdParty/trident"}>
-            <div className="bg-[#007A37] text-white text-xs sm:text-base px-4 py-4 sm:px-4 sm:py-2 uppercase rounded-md">
-              select
+        <div className="opts w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-8">
+          <Link href={"/service/psvBodaboda/thirdParty/1m"}>
+            <div className="py-2 uppercase text-white bg-[#007A37] grid place-items-center">
+              1 month
             </div>
           </Link>
-
-          <div className="h-16 w-20 relative">
-            <Image
-              src={"/partners/directline.png"}
-              alt={"Directline"}
-              fill
-              priority
-              className="w-full h-full"
-            />
-          </div>
-          <div className="dark:text-gray-900">4, 500</div>
-          <Link href={"/service/psvBodaboda/thirdParty/directline"}>
-            <div className="bg-[#007A37] text-white text-xs sm:text-base px-4 py-4 sm:px-4 sm:py-2 uppercase rounded-md">
-              select
+          <Link href={"/service/psvBodaboda/thirdParty/6m"}>
+            <div className="py-2 uppercase text-white bg-[#E41313] grid place-items-center">
+              6 months
+            </div>
+          </Link>{" "}
+          <Link href={"/service/psvBodaboda/thirdParty/1yr"}>
+            <div className="py-2 uppercase text-white bg-[#000000] grid place-items-center">
+              1 year
             </div>
           </Link>
         </div>
